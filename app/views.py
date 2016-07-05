47e915e8a9841a403603cbb4a401815bbe7266d3
chucklapress <charleslapress@gmail.com>
2016-07-05 18:40:57 -0400

    worked with Bill and John to resolve changing to single view because pulling two different tables

--- app/views.py
+++ app/views.py
@@ -2,24 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+
 
 
 # Create your views here.
-def get_stats(player):
-    url = "http://www.nfl.com/player/profile{}".format(player)
+def player_scraping_view(request):
+    player = request.GET.get('player') or "Drew Brees"
+    url = "http://www.nfl.com/search?query={}".format(player)
     content = requests.get(url).text
     souper = BeautifulSoup(content, "html.parser")
-    current_player = souper.find(class_="stats")
-    player_url = current_player.a.attrs["href"]
-    content = requests.get(player_url).text
-    return current_player
+    current_player = souper.find(class_="stats").attrs["href"]
+    content = requests.get(current_player).text
+    souper = BeautifulSoup(content, "html.parser")
+    player_stats = str(souper.find(id="main-content"))
+    return render(request,"index.html",{"current_player": current_player,"player": player, "player_stats": player_stats})
 
 
-def player_scraping_view(request):
-    player = request.GET.get('player') or "Brett Farve"
-    current_player = get_stats(player)
+
+#def player_scraping_view(request):
+    #player = request.GET.get('player') or "Drew Brees"
+    #current_player = get_stats(player)
     #player_id = urlparse(current_player.attrs("href")).query.strip("id")
     #content = requests.get('http://www.nfl.com/player/drewbrees/2504775/profile')
     #souper = BeautifulSoup(content.text, "html.parser")
-    return render(request,"index.html", {"current_player": current_player,"player": player})
+    #return render(request,"index.html",{"current_player": current_player,"player": player})
